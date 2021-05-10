ee0feba76674be3b67af0b678d181eb919678f5c
kfabo <kevenarcel.fabo@factset.com>
2021-05-10 21:13:33 +0200

    choose preprocess based on the model version

--- api/ml/preprocess.py
+++ api/ml/preprocess.py
@@ -1,93 +1,144 @@
 import os
 import pandas as pd
-from utils import CreditRiskException
 from sklearn.pipeline import FeatureUnion, Pipeline
 from sklearn.preprocessing import StandardScaler
+from .utils import CreditRiskException
 
-
+from .config import Settings
 
 # use utf-8 as the encoding
-encoding = 'utf-8'
+encoding = "utf-8"
+
 
 def get_cat_columns():
     """ Method to get all categorical colummns for the credit risk dataset """
 
-    columns_cat = ['checking_status', 'employment', 'credit_history', 'purpose', 'savings_status', 
-                    'sex', 'status_matrimonial', 'other_parties', 'property_magnitude', 'other_payment_plans', 
-                    'housing','job','own_telephone','foreign_worker','class']
+    columns_cat = [
+        "checking_status",
+        "employment",
+        "credit_history",
+        "purpose",
+        "savings_status",
+        "sex",
+        "status_matrimonial",
+        "other_parties",
+        "property_magnitude",
+        "other_payment_plans",
+        "housing",
+        "job",
+        "own_telephone",
+        "foreign_worker",
+        "class",
+    ]
     return columns_cat
+
 
 def get_num_columns():
     """ Method to get all numerical colummns for the credit risk dataset """
-    
-    columns_num = ['duration', 'credit_amount', 'installment_commitment', 'residence_since', 'age', 'existing_credits',
-                    'num_dependents']
+
+    columns_num = [
+        "duration",
+        "credit_amount",
+        "installment_commitment",
+        "residence_since",
+        "age",
+        "existing_credits",
+        "num_dependents",
+    ]
     return columns_num
 
-class CreditRisk_Preprocess():
+
+class CreditRisk_Preprocess:
     def __init__(self):
         self.training_data_folder = ""
-        self.columns_type = {'checking_status': 'str', 'employment': 'str', 'credit_history': 'str', 'purpose': 'str', 'savings_status': 'str', 'sex': 'str'
-                            , 'status_matrimonial': 'str', 'other_parties': 'str', 'property_magnitude': 'str', 'other_payment_plans': 'str'
-                            , 'housing': 'str', 'job': 'str', 'own_telephone': 'str', 'foreign_worker': 'str', 'class': 'str', 'duration': 'int', 'credit_amount': 'int'
-                            , 'installment_commitment': 'int', 'residence_since': 'int', 'age': 'int', 'existing_credits': 'int', 'num_dependents': 'int'}
+        self.columns_type = {
+            "checking_status": "str",
+            "employment": "str",
+            "credit_history": "str",
+            "purpose": "str",
+            "savings_status": "str",
+            "sex": "str",
+            "status_matrimonial": "str",
+            "other_parties": "str",
+            "property_magnitude": "str",
+            "other_payment_plans": "str",
+            "housing": "str",
+            "job": "str",
+            "own_telephone": "str",
+            "foreign_worker": "str",
+            "class": "str",
+            "duration": "int",
+            "credit_amount": "int",
+            "installment_commitment": "int",
+            "residence_since": "int",
+            "age": "int",
+            "existing_credits": "int",
+            "num_dependents": "int",
+        }
 
-    self.missing_values = ["n/a", "na", "--","NaN","nan","N/A"]
+    self.missing_values = ["n/a", "na", "--", "NaN", "nan", "N/A"]
 
     def load_data(self):
         """ Method to load credit risk dataset for further processing """
 
         try:
-            # data path 
-            path_data = os.path.join(self.training_data_folder, 'credit_data.csv')
+            # data path
+            path_data = os.path.join(self.training_data_folder, "credit_data.csv")
 
-            # load data in a dataframe 
-            df_train = pd.read_csv(path_data, encoding=encoding, na_values=self.missing_values)
+            # load data in a dataframe
+            df_train = pd.read_csv(
+                path_data, encoding=encoding, na_values=self.missing_values
+            )
 
             # update columns data type
             df_train = df_train.astype(self.columns_type)
 
         except Exception as e:
-            raise CreditRiskException('load_data', str(e))
+            raise CreditRiskException("load_data", str(e))
 
         return df_train
 
     def _cat_pipeline_preprocess(self, cat_attribs):
 
-        cat_pipeline = Pipeline([
-                ('selector', DataFrameSelector(cat_attribs)),
-                ('cat_encoder', OneHotEncoder(sparse=False)),
-            ])
+        cat_pipeline = Pipeline(
+            [
+                ("selector", DataFrameSelector(cat_attribs)),
+                ("cat_encoder", OneHotEncoder(sparse=False)),
+            ]
+        )
 
         return cat_pipeline
 
     def _num_pipeline_preprocess(self, num_attribs, use_scaler):
 
-        num_pipeline = Pipeline([
-                ('selector', DataFrameSelector(num_attribs)),
-                ('imputer', SimpleImputer(strategy="median")),
-                ('attribs_adder', CombinedAttributesAdder())
-            ])
+        num_pipeline = Pipeline(
+            [
+                ("selector", DataFrameSelector(num_attribs)),
+                ("imputer", SimpleImputer(strategy="median")),
+                ("attribs_adder", CombinedAttributesAdder()),
+            ]
+        )
 
         if use_scaler:
             # add a scaling step in the num pipeline
-            num_pipeline.steps.append(['std_scaler', StandardScaler()])
+            num_pipeline.steps.append(["std_scaler", StandardScaler()])
 
         return num_pipeline
 
     def full_pipeline_preprocess(self, num_attribs, cat_attribs, use_scaler):
-            
-        full_pipeline = FeatureUnion(transformer_list=[
-                ("num_pipeline", self._num_pipeline_preprocess(num_attribs, use_scaler))
+
+        full_pipeline = FeatureUnion(
+            transformer_list=[
+                ("num_pipeline", self._num_pipeline_preprocess(num_attribs, use_scaler)),
                 ("cat_pipeline", self._cat_pipeline_preprocess(cat_attribs)),
-            ])
+            ]
+        )
 
         return full_pipeline
 
     def prepare_input_data(self):
         """ Prepare data to be use to predict or train using credit data """
-        return 
-
+        return
 
 
 class CreditRisk_Preprocess_Train(CreditRisk_Preprocess):
@@ -102,7 +153,8 @@ class CreditRisk_Preprocess_Train(CreditRisk_Preprocess):
     def _save_preprocess_pipeline():
         """ Method to save the preprocessing pipeline """
 
-        return 
+        return
+
 
 class CreditRisk_Preprocess_Predict(CreditRisk_Preprocess):
     def __init__(self, df_input):
@@ -113,18 +165,18 @@ class CreditRisk_Preprocess_Predict(CreditRisk_Preprocess):
 
         # get the preprocess pipeline path
 
-        # load the pickle object 
-            # raise an exception if can't load the pipe
+        # load the pickle object
+        # raise an exception if can't load the pipe
 
-        return 
+        return
 
     def prepare_input_features(self, df_input, use_scaler):
 
         # load pipe
 
-        # do a transform 
+        # do a transform
 
-        # return the preprocess df on inputs 
-        df_input_features = None 
+        # return the preprocess df on inputs
+        df_input_features = None
 
-        return 
+        return
--- api/ml/utils.py
+++ api/ml/utils.py
@@ -1,51 +1,56 @@
 import json
+
 from sklearn.base import BaseEstimator, TransformerMixin
 
 ##################################################
 # CATCH ERRORS
 ##################################################
 
-# class to raise credit risk exceptions 
+# class to raise credit risk exceptions
 class CreditRiskException(Exception):
     def __init__(self, message, errors):
 
         jsonresult = {}
-        jsonresult['error_title'] = message
-        jsonresult['error'] = errors
+        jsonresult["error_title"] = message
+        jsonresult["error"] = errors
 
-        # call the base class constructor 
+        # call the base class constructor
         if sys.version_info[0] < 3:
             super(CreditRiskException, self).__init__(json.dumps(jsonresult))
         else:
             super().__init__(json.dumps(jsonresult))
 
+
 ##################################################
-# PREPROCESS TRANSFORMERS 
+# PREPROCESS TRANSFORMERS
 ##################################################
 # todo
 # 1. remove class column if it exists
 # 2. apply data type of columns
-# 3. extract new features 
+# 3. extract new features
 # 4. handle unknown values
-# 6. Imputer  
+# 6. Imputer
 
 # 7. normalize numerical features
-# 8. create pipeline for each models 
+# 8. create pipeline for each models
 
-# cat and num encoder 
+# cat and num encoder
 
-# Remove target column  
+# Remove target column
 class RemoveTargetColumn(BaseEstimator, TransformerMixin):
     def __init__(self, attribute_names):
         self.attribute_names = attribute_names
+
     def fit(self, X, y=None):
         return self
+
     def transform(self, X):
         return X[self.attribute_names].values
 
-# Apply data types on columns 
+
+# Apply data types on columns
 class RemoveTargetColumn(BaseEstimator, TransformerMixin):
-    def __init__(self, add_bedrooms_per_room=True): # no *args or **kargs
+    def __init__(self, add_bedrooms_per_room=True):  # no *args or **kargs
         self.add_bedrooms_per_room = add_bedrooms_per_room
 
     def fit(self, X, y=None):
@@ -54,9 +59,10 @@ class RemoveTargetColumn(BaseEstimator, TransformerMixin):
     def transform(self, X):
         return
 
-# Extract new features  
+
+# Extract new features
 class RemoveTargetColumn(BaseEstimator, TransformerMixin):
-    def __init__(self, add_bedrooms_per_room=True): # no *args or **kargs
+    def __init__(self, add_bedrooms_per_room=True):  # no *args or **kargs
         self.add_bedrooms_per_room = add_bedrooms_per_room
 
     def fit(self, X, y=None):
@@ -64,10 +70,11 @@ class RemoveTargetColumn(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         return
+
 
 # Handle unknown values
 class RemoveTargetColumn(BaseEstimator, TransformerMixin):
-    def __init__(self, add_bedrooms_per_room=True): # no *args or **kargs
+    def __init__(self, add_bedrooms_per_room=True):  # no *args or **kargs
         self.add_bedrooms_per_room = add_bedrooms_per_room
 
     def fit(self, X, y=None):
@@ -76,29 +83,38 @@ class RemoveTargetColumn(BaseEstimator, TransformerMixin):
     def transform(self, X):
         return
 
-# Create a class to select numerical or categorical columns 
+
+# Create a class to select numerical or categorical columns
 class DataFrameSelector(BaseEstimator, TransformerMixin):
     def __init__(self, attribute_names):
         self.attribute_names = attribute_names
+
     def fit(self, X, y=None):
         return self
+
     def transform(self, X):
         return X[self.attribute_names].values
 
-# Imputer 
+
+# Imputer
 class DataFrameSelector(BaseEstimator, TransformerMixin):
     def __init__(self, attribute_names):
         self.attribute_names = attribute_names
+
     def fit(self, X, y=None):
         return self
+
     def transform(self, X):
         return X[self.attribute_names].values
 
-# encoding 
+
+# encoding
 class DataFrameSelector(BaseEstimator, TransformerMixin):
     def __init__(self, attribute_names):
         self.attribute_names = attribute_names
+
     def fit(self, X, y=None):
         return self
+
     def transform(self, X):
         return X[self.attribute_names].values
