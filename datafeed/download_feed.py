40cf633d5f1c6b5e30f4668eb60273e3853cb6d7
Chris Tene <tenedjomo@gmail.com>
2021-05-16 18:41:52 +0100

    Update download_feed.py

--- datafeed/download_feed.py
+++ datafeed/download_feed.py
@@ -2,32 +2,29 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import os
-
-
+from pathlib import Path
 
 def download_feed(target_folder): 
     try:
         #download dataset
         data = pd.read_csv('https://datahub.io/machine-learning/credit-g/r/credit-g.csv')
         
-        #split dataset
-        train,test = train_test_split(data, test_size=0.2, random_state=42)
-    
-        target_folder = 'C:\\Users\\HP\\Desktop\\datafeed\\data\\'
+        target_folder = Path(__file__).parent / "data"
 
-        train.to_csv(os.target_folder.join(target_folder, r'train.csv'))
-        test.to_csv(os.target_folder.join(target_folder, r'test.csv'))
+        #split dataset
+        train, test = train_test_split(data, test_size=0.2, random_state=42)
+        
+        train.to_csv(target_folder/ "train.csv")
+        
+        test.to_csv(target_folder/ "test.csv")
 
         print("Download succeeded")
 
     except Exception as e:
         print(f"Failed download : {e}")
     
-    
-
-
 if __name__ == '__main__':
-    download_feed()
+    download_feed('target_folder')
 
 
 
