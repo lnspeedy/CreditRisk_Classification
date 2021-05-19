88d6d099948c59c82919a234f17dc52343e23bee
Chris Tene <tenedjomo@gmail.com>
2021-05-19 23:04:24 +0100

    download_feed

---
+++ datafeed/download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#creation of the environment variable. You will need to specify the path of your target folder in the ribs
+export DOWNLOAD_FEED='' 
+
+#determination of the execution rights of the python script
+chmod +x download_feed.py
+
+#python script execution
+python download_feed.py
+    
+
+
+
+
+
--- datafeed/download_feed.py
+++ datafeed/download_feed.py
@@ -2,29 +2,27 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import os
-from pathlib import Path
+import subprocess
 
-def download_feed(target_folder): 
+
+def download_feed(target_folder):
     try:
         #download dataset
         data = pd.read_csv('https://datahub.io/machine-learning/credit-g/r/credit-g.csv')
-        
-        target_folder = Path(__file__).parent / "data"
 
         #split dataset
         train, test = train_test_split(data, test_size=0.2, random_state=42)
         
-        train.to_csv(target_folder/ "train.csv")
-        
-        test.to_csv(target_folder/ "test.csv")
+        #save new datasets after split to target folder
+        train.to_csv(os.path.join(target_folder, "train.csv"))
+        test.to_csv(os.path.join(target_folder, "test.csv"))
 
         print("Download succeeded")
 
     except Exception as e:
-        print(f"Failed download : {e}")
-    
+        print("Failed download :", e)
+
+#execution of python script and specified the environment variable       
 if __name__ == '__main__':
-    download_feed('target_folder')
-
-
-
+    target_folder = os.getenv('DOWNLOAD_FEED') 
+    download_feed(target_folder) 
