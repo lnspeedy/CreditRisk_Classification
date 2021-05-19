18973ea452afd174ee0cd3930307304f7fae34a6
Chris Tene <tenedjomo@gmail.com>
2021-05-19 20:57:29 +0100

    chris

---
+++ datafeed/download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#!/bin/env python
+runn(){
+    export Var_env='/mnt/c/Users/HP/Desktop/datafeed/data'
+    chmod +x download_feed.py
+    python ./download_feed.py
+}
+runn
+
+
+
+
--- datafeed/download_feed.py
+++ datafeed/download_feed.py
@@ -2,29 +2,30 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import os
-from pathlib import Path
+import subprocess
 
 def download_feed(target_folder): 
     try:
         #download dataset
         data = pd.read_csv('https://datahub.io/machine-learning/credit-g/r/credit-g.csv')
-        
-        target_folder = Path(__file__).parent / "data"
 
         #split dataset
         train, test = train_test_split(data, test_size=0.2, random_state=42)
         
-        train.to_csv(target_folder/ "train.csv")
+        train.to_csv(os.path.join(target_folder, "train.csv"))
         
-        test.to_csv(target_folder/ "test.csv")
+        test.to_csv(os.path.join(target_folder, "test.csv"))
 
         print("Download succeeded")
 
     except Exception as e:
-        print(f"Failed download : {e}")
+        print("Failed download :", e)
+0
+      
     
 if __name__ == '__main__':
-    download_feed('target_folder')
-
+    env = os.getenv('Var_env')
+    target_folder = env
+    download_feed(target_folder)
 
 
